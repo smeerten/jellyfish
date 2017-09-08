af0ad03448439ac003c84e1e6f21a223144c571c
wfranssen <wmjfranssen@zoho.com>
2017-09-08 14:43:15 +0200

    Added license

--- safeEval.py
+++ safeEval.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
-# Copyright 2016 - 2017 Bas van Meerten and Wouter Franssen
+# Copyright 2017 Wouter Franssen and Bas van Meerten
 
-# This file is part of ssNake.
+# This file is part of Jellyfish.
 #
-# ssNake is free software: you can redistribute it and/or modify
+# Jellyfish is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ssNake is distributed in the hope that it will be useful,
+# Jellyfish is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with ssNake. If not, see <http://www.gnu.org/licenses/>.
+# along with Jellyfish. If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
 import re
